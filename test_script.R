c5ee5249203a4982b55bfd11c43721c3edb4ccd5
张竹清 <zhangzhuqing@zzqs-macbook-3.mynet>
2020-11-09 18:41:10 +0000

    back_in_time_1

--- test_script.R
+++ test_script.R
@@ -8,3 +8,16 @@ z
 
 #hi can you see it?
 #hi yes i can see!
+
+install.packages("reprex")
+library(reprex)
+
+a<-1
+b<-2
+c<-a+b
+c
+
+reprex()
+reprex(venue="so")
+rlang::last_error()
+rlang::last_trace()
